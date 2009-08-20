fca8381600e0a6e73d8fca07ae90ff92212294c4
Steve Kondik <shade@chemlab.org>
2009-08-19 20:40:13 -0400

    Who turned on tracing anyway?

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -169,7 +169,7 @@ public class ComposeMessageActivity extends Activity
 
     private static final String TAG = "ComposeMessageActivity";
     private static final boolean DEBUG = true;
-    private static final boolean TRACE = true;
+    private static final boolean TRACE = false;
     private static final boolean LOCAL_LOGV = DEBUG ? Config.LOGD : Config.LOGV;
 
     // Menu ID
